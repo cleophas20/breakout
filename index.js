0b16999ab622287487fbdd152c33f887ec3ddc35
Cleophas <cleophasfoy@gmail.com>
2022-09-07 14:10:34 -0500

    added user and created start position

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const grid = document.querySelector('.grid')
 const blockWidth = 100
 const blockHeight = 20
 
+const userStart = [230, 10]
+let currentPosition = userStart
+
 //create block
 class Block {
   constructor(xAxis, yAxis) {
@@ -41,5 +44,13 @@ function addBlocks() {
     grid.appendChild(block)
   }
 }
-
 addBlocks()
+
+//add user
+
+const user = document.createElement('div')
+user.classList.add('user')
+user.style.left = currentPosition[0] + 'px'
+user.style.bottom = currentPosition[1] + 'px'
+grid.appendChild(user)
+ 
